598ed0cf2f5cc2ff0d991ecae4f4a40ef04d9c4a
hansent <thomas.hansen@gmail.com>
2014-10-11 17:50:36 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var envify = require('envify/custom');
+var partialify = require('partialify');
+var sourcemaps = require('gulp-sourcemaps');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var rimraf = require('gulp-rimraf');
+var notify = require('gulp-notify');
+var markdown = require('gulp-markdown');
+var browserSync = require('browser-sync');
+var reactify = require('reactify');
+
+// default task /////////////////////////////////////////////////
+gulp.task('default', ['build'], function() {
+    browserSync({
+        tunnel: true,
+        open: false,
+        files: ['./build/**/*', '!./build/lib/**/*'],
+        ghostMode: false,
+        reloadDelay: 200,
+        notify: false,
+        server: {
+            baseDir: './build',
+        },
+    });
+
+    gulp.watch('public/**/*.js', ['browserify']);
+    gulp.watch('public/**/*.html', ['files']);
+    gulp.watch('public/**/*.md', ['markdown']);
+    gulp.watch('public/**/*.css', ['css']);
+    gulp.watch('public/**/*.less', ['less']);
+
+});
+
+gulp.task('clean', function() {
+    return gulp.src(['build/**/*'], {
+        read: false
+    }).pipe(rimraf());
+});
+
+
+
+
+
+
+// build tasks //////////////////////////////////////////////////
+gulp.task('build', ['browserify']);
+
+gulp.task('browserify', ['files'], function() {
+    var environ = {
+        NODE_ENV: process.env.NODE_ENV
+    };
+
+    browserify()
+        .add('./public/main.js')
+        .require('react')
+        .transform(envify(environ))
+        .transform(partialify)
+        .transform(reactify)
+        .bundle({
+            debug: process.env.NODE_ENV != 'production'
+        })
+        .on('error', function(err) {
+            notify.onError('Error: <%= error.message %>')(err);
+            this.end();
+        })
+        .pipe(source('index.js'))
+        .pipe(gulp.dest('build/'));
+});
+
+
+
+// assets //////////////////////////////////////////////////////
+gulp.task('files', ['css', 'markdown'], function() {
+    gulp.src(['./public/**/*'])
+        .pipe(gulp.dest('public/'));
+});
+
+
+gulp.task('markdown', function() {
+    return gulp.src(['./public/**/*.md'])
+        .pipe(markdown())
+        .pipe(gulp.dest('public/'));
+});
+
+
+gulp.task('css', ['less'], function() {
+    return gulp.src(['public/**/*.css'])
+        .pipe(autoprefixer('last 1 version'))
+        .pipe(gulp.dest('./build'))
+});
+
+gulp.task('less', function() {
+    var less_transform = less();
+    less_transform.on('error', function(err) {
+        notify.onError('<%= error.message %>')(err);
+        this.end();
+    });
+    return gulp.src('./public/**/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(less_transform)
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./build'));
+});
+
+
+gulp.task('favicon', function() {
+    return gulp.src('public/img/favicon.ico')
+        .pipe(gulp.dest('build/'));
+});
---
+++ public/record.md
@@ -0,0 +1,109 @@
+# John James Doe, Jr.
+## 60yrs Hispanic Male
+## DOB: 2/24/1954
+
+<img style="width: 25%" src='img/bill.png'>
+
+## Medications / indication:
+- **Lexapro** 20 mg PO daily _Depression_
+- **Aspirin** 81mg PO daily _Heart Health_
+- **Hydrochlorthiazide** 12.5mg PO daily _Hypertension_
+- **Levitra** 10mg PO daily _Erectile Dysfunction/Prostate Hypertrophy_
+- **Methotrexate** 3.3 mg/m^2 IM 2x/wk PRN Lupus Flare _Lupus_
+- **Sumitriptan** 6 mg SQ x1, may repeat dose after 1 hour _Cluster Headache_
+
+
+## Past Medical History
+- Hypertension
+- Diabetes  - Type II
+- Obesity
+- History of Recurrent Depression
+- Erectile Dysfunction
+- Lupus
+- Endocarditis
+- Cluster Headaches
+- Impetigo
+- Colon Polyps
+- Prostate Hypertrophy
+- History of Rubella
+- Post Traumatic Stress Disorder
+- Low Back Pain
+
+## Past Surgical History
+- **Apendectomy** _2002_ (Mercy Medical Center, Iowa City, IA)
+- **Colonoscopy** _2011_ (St. Lukes, Cedar Rapids, IA)
+- **Cholecystechtomy** _2001_ (St. Lukes Cedar Rapids, IA)
+- **Bowel Resection** _1999_ University of Iowa, Iowa City, IA)
+
+## Family History
+<img src='img/genogram.png'>
+
+## Social History
+- Brick Layer
+- Married
+- Education: Some College
+- Former Smoker—1 PPD x 25 years
+- Alcohol Use—Occasional
+- Denies illicit drug use
+- Exercise: No
+- Diet:  Vegetarian
+- Military: Vietnam War—Honorably Discharged
+- Travel: None Recent
+- Venereal Diseases:  None
+
+## Immunizations
+- **Influenza** _2013_
+- **Influenza** _2014_
+- **Zoster-** _2012_
+- **Pneumonia** _2009_
+
+
+## Emergency
+- Code Status:  DNR
+- Blood Type: 0+
+- Organ Donor: Yes
+- Language: Spanish, English
+- Religious Preference:
+- Jehovah Witness
+- Power of Attorney (POA):
+- Name: Jacob Doe (Son)
+- Living Will: No
+
+
+## Allergies/Reaction
+- **Penicillin** (Rash)
+
+
+
+## Physician
+**Catherine Hansen, DO**
+	North Liberty Family Clinic Center
+	1200 Delaney Court
+	North Liberty, IA 52317
+	Phone:  319.626.5555
+	Fax:  319.626.5554
+
+## Pharmacy
+**Walgreens**
+	1200 Delaney Court
+	North Liberty, IA 52317
+	Phone:  319.626.5555
+	Fax:  319.626.5554
+
+
+
+## Insurance
+- Blue Cross Blue Shield of Alabama
+- Medicare
+- Dental—Delta Dental
+- Eye—VSP
+- Auto: Progressive
+- Life: Northwestern
+
+
+
+
+
+
+
+
